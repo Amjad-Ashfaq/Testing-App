00bfc7898be2ecf081382a769f2034a092e05706
Amjad-Ashfaq <bsef18a543@pucit.edu.pk>
2021-11-09 10:39:09 -0800

    layouts updated

--- testing-app/app/src/main/java/com/example/myapplication/MainActivity.java
+++ testing-app/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    Button btn_repo = (Button)findViewById(R.id.button3);
+    btn_repo.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            // TODO Auto-generated method stub
+            String url = "http://www.gobloggerslive.com";
+
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        }
+    Uri uri = Uri.parse("http://www.google.com"); // missing 'http://' will cause crashed
+    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+    startActivity(intent);
+}
 }
--- testing-app/app/src/main/java/com/example/myapplication/Quiz_activity.java
+++ testing-app/app/src/main/java/com/example/myapplication/Quiz_activity.java
@@ -11,4 +11,5 @@ public class Quiz_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
     }
+
 }
