0331137e937ab0038e37faf1e251ddb8b30db474
Amjad-Ashfaq <bsef18a543@pucit.edu.pk>
2021-11-11 20:25:58 +0500

    Quiz questions and options added

--- testing-app/app/src/main/java/com/example/myapplication/Quiz_activity.java
+++ testing-app/app/src/main/java/com/example/myapplication/Quiz_activity.java
@@ -14,20 +14,29 @@ public class Quiz_activity extends AppCompatActivity {
     Button opt1, opt2, opt3, opt4, next_btn;
     TextView ques, score_val;
     int i = 0, score=0;
-    String question[] = {"quest1", "quest2"};
-    String options[][] = {{"A", "B", "C", "D"}, {"A", "B", "C", "D"}};
-    char selected[]={0};
-    char answer[] = {'C', 'B'};
+    String question[] = {"1. To pronounce halqiyah huruf, sound produced from:",
+            "2. To pronounce lahatiyah huruf, sound produced from:",
+            "3. To pronounce neetiyah huruf, sound produced from:",
+            "4. To pronounce lisaveyah huruf, sound produced from:",
+            "5. To pronounce tarfiyah huruf, sound produced from:"};
+    String options[][] = {{"A) End of throat", "B) Middle of throat", "C) Start of throat", "D) All of these"},
+                          {"A) Base of tongue near Uvula", "B) Portion of tongue near its base", "C) both a and b", "D) none of these"},
+                          {"A) Tip of the tongue","B) Middle of tongue","C) Base of the tongue","D) None of these"},
+                          {"A) Tip of the tongue touching the tip of the frontal 2 teeth","B) Tip of the tongue comes between the front top and bottom teeth","C) Both a and b","D) none of these"},
+                          {"A) Rounded tip of the tongue touching the base of the frontal 8 teeth","B) Rounded tip of the tongue touching the base of the frontal 6 teeth","C) Rounded tip of the tongue touching the base of the frontal 4 teeth","D) All of these"}};
+
+    char selected[]={0,0,0,0,0};
+    char answer[] = {'D', 'C','A','C', 'D'};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        loadUI();
+        clickFunc();
         myview(i);
     }
 
     void loadUI() {
-
-
         opt1 = (Button) findViewById(R.id.opt1);
         opt2 = (Button) findViewById(R.id.opt2);
         opt3 = (Button) findViewById(R.id.opt3);
